26b0f8afcd851b8314fabdce42aabea4bcbaeffa
David DEKEUWER <david.dekeuwer@gmail.com>
2021-03-04 22:53:31 +0100

    add DateUtils

--- app/src/main/java/fr/feepin/maru/data/local/FakeMeetingApiGenerator.java
+++ app/src/main/java/fr/feepin/maru/data/local/FakeMeetingApiGenerator.java
@@ -9,6 +9,7 @@ import java.util.Random;
 
 import fr.feepin.maru.models.Meeting;
 import fr.feepin.maru.models.Room;
+import fr.feepin.maru.utils.DateUtils;
 
 public class FakeMeetingApiGenerator {
 
@@ -32,15 +33,6 @@ public class FakeMeetingApiGenerator {
         return new ArrayList<Meeting>(Arrays.asList(fakeMeetings));
     }
 
-    public static long getTimeInMillis(int hours, int minutes) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, hours);
-        calendar.set(Calendar.MINUTE, minutes);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-        return calendar.getTimeInMillis();
-    }
-
     public static List<String> generateRandomParticipants(int numberOfParticipants) {
         ArrayList<String> emails = new ArrayList<>();
 
@@ -54,14 +46,14 @@ public class FakeMeetingApiGenerator {
     }
 
     public static Meeting[] fakeMeetings = {
-            new Meeting(1, getTimeInMillis(17, 45), Room.LUIGI, "Stocks", generateRandomParticipants(6)),
-            new Meeting(2, getTimeInMillis(8, 00), Room.WARIO, "Daily", generateRandomParticipants(2)),
-            new Meeting(3, getTimeInMillis(14, 20), Room.YOSHI, "Etat des lieux", generateRandomParticipants(3)),
-            new Meeting(4, getTimeInMillis(9, 30), Room.MARIO, "Marketing", generateRandomParticipants(4)),
-            new Meeting(5, getTimeInMillis(16, 50), Room.PEACH, "Projet 23", generateRandomParticipants(2)),
-            new Meeting(6, getTimeInMillis(8, 30), Room.TOAD, "Briefing", generateRandomParticipants(8)),
-            new Meeting(7, getTimeInMillis(15, 00), Room.WALUIGI, "Brainstorming", generateRandomParticipants(6)),
-            new Meeting(8, getTimeInMillis(19, 20), Room.DK, "Review", generateRandomParticipants(2)),
+            new Meeting(1, DateUtils.getDateMillisFromTime(17, 45), Room.LUIGI, "Stocks", generateRandomParticipants(6)),
+            new Meeting(2, DateUtils.getDateMillisFromTime(8, 00), Room.WARIO, "Daily", generateRandomParticipants(2)),
+            new Meeting(3, DateUtils.getDateMillisFromTime(14, 20), Room.YOSHI, "Etat des lieux", generateRandomParticipants(3)),
+            new Meeting(4, DateUtils.getDateMillisFromTime(9, 30), Room.MARIO, "Marketing", generateRandomParticipants(4)),
+            new Meeting(5, DateUtils.getDateMillisFromTime(16, 50), Room.PEACH, "Projet 23", generateRandomParticipants(2)),
+            new Meeting(6, DateUtils.getDateMillisFromTime(8, 30), Room.TOAD, "Briefing", generateRandomParticipants(8)),
+            new Meeting(7, DateUtils.getDateMillisFromTime(15, 00), Room.WALUIGI, "Brainstorming", generateRandomParticipants(6)),
+            new Meeting(8, DateUtils.getDateMillisFromTime(19, 20), Room.DK, "Review", generateRandomParticipants(2)),
     };
 
 }
---
+++ app/src/main/java/fr/feepin/maru/utils/DateUtils.java
@@ -0,0 +1,14 @@
+package fr.feepin.maru.utils;
+
+import java.util.Calendar;
+
+public class DateUtils {
+    public static long getDateMillisFromTime(int hours, int minutes) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hours);
+        calendar.set(Calendar.MINUTE, minutes);
+        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.set(Calendar.SECOND, 0);
+        return calendar.getTimeInMillis();
+    }
+}
