783f0fe877beda9eac93ae1eeba1f0e26fceb3ff
David DEKEUWER <david.dekeuwer@gmail.com>
2021-03-12 18:24:14 +0100

    feat: add FilterDialog a custom dialog

---
+++ app/src/main/java/fr/feepin/maru/dialogs/FilterDialog.java
@@ -0,0 +1,121 @@
+package fr.feepin.maru.dialogs;
+
+import android.content.DialogInterface;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.DialogFragment;
+
+import com.google.android.material.chip.Chip;
+import com.google.android.material.slider.RangeSlider;
+
+import java.util.List;
+
+import fr.feepin.maru.R;
+import fr.feepin.maru.adapters.RoomsChipAdapter;
+import fr.feepin.maru.data.local.FakeMeetingApi;
+import fr.feepin.maru.data.local.MeetingApi;
+import fr.feepin.maru.databinding.DialogFilterBinding;
+import fr.feepin.maru.models.MeetingListFilterData;
+import fr.feepin.maru.models.Room;
+import fr.feepin.maru.presenters.FilterDialogMvpPresenter;
+import fr.feepin.maru.presenters.FilterDialogPresenter;
+import fr.feepin.maru.views.FilterDialogMvpView;
+
+public class FilterDialog extends DialogFragment implements FilterDialogMvpView, RoomsChipAdapter.OnRoomSelectListener {
+
+    private DialogFilterBinding binding;
+    private RoomsChipAdapter roomsChipAdapter;
+    private FilterDialogMvpPresenter presenter;
+    private OnFilterDataReceiveListener onFilterDataReceiveListener;
+
+    public FilterDialog() {
+        presenter = new FilterDialogPresenter(FakeMeetingApi.getInstance());
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DialogFilterBinding.inflate(inflater, container, false);
+        roomsChipAdapter = new RoomsChipAdapter(binding.chipGroupRooms, this);
+        presenter.onAttachView(this);
+
+        binding.sliderTime.addOnChangeListener(new RangeSlider.OnChangeListener() {
+            @Override
+            public void onValueChange(@NonNull RangeSlider slider, float value, boolean fromUser) {
+                int fromHour = (int) (float) slider.getValues().get(0);
+                int toHour = (int) (float) slider.getValues().get(1);
+                presenter.onStartingTimeChange(fromHour);
+                presenter.onEndingTimeChange(toHour);
+
+                binding.tvTime.setText(String.format("%s-%s", fromHour, toHour));
+            }
+        });
+
+        binding.btnApply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.onApplyButtonClick();
+                dismiss();
+            }
+        });
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        presenter.onDetachView();
+        roomsChipAdapter = null;
+        binding = null;
+    }
+
+    @Override
+    public void onDismiss(@NonNull DialogInterface dialog) {
+        super.onDismiss(dialog);
+        presenter.onDismissDialog();
+    }
+
+    @Override
+    public void onRoomSelect(Room room) {
+        presenter.onRoomSelect(room);
+    }
+
+    @Override
+    public void onRoomUnselect(Room room) {
+        presenter.onRoomUnselect(room);
+    }
+
+    @Override
+    public void setTimeSlider(int startingHours, int endingHours) {
+        binding.sliderTime.setValues((float) startingHours, (float) endingHours);
+    }
+
+    public void setOnFilterDataReceiveListener(OnFilterDataReceiveListener onFilterDataReceiveListener) {
+        this.onFilterDataReceiveListener = onFilterDataReceiveListener;
+    }
+
+    @Override
+    public void sendFilterData(MeetingListFilterData filterData) {
+        if (onFilterDataReceiveListener != null) {
+            onFilterDataReceiveListener.onFilterDataReceive(filterData);
+        }
+    }
+
+    public interface OnFilterDataReceiveListener {
+        void onFilterDataReceive(MeetingListFilterData filterData);
+    }
+}
