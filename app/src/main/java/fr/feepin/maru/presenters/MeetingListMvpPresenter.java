2b1d1f401b6edc7cc4e5f499ac61ec4ce7fe16e0
David DEKEUWER <david.dekeuwer@gmail.com>
2021-03-12 18:22:27 +0100

    improvement: remove MeetingListMvpPresenter methods that are now handled by the FilterDialogPresenter

--- app/src/main/java/fr/feepin/maru/presenters/MeetingListMvpPresenter.java
+++ app/src/main/java/fr/feepin/maru/presenters/MeetingListMvpPresenter.java
@@ -1,13 +1,12 @@
 package fr.feepin.maru.presenters;
 
 import fr.feepin.maru.models.Meeting;
+import fr.feepin.maru.models.MeetingListFilterData;
 import fr.feepin.maru.models.Room;
 import fr.feepin.maru.views.MeetingListMvpView;
 
 public interface MeetingListMvpPresenter extends MvpPresenter<MeetingListMvpView> {
     void onDeleteMeetingIconClick(Meeting meeting);
     void onFilterIconClick();
-    void onRangeTimeChange(long startingTimeMillis, long endingTimeMillis);
-    void onRoomSelect(Room room);
-    void onRoomUnselect(Room room);
+    void onFilterDataReceive(MeetingListFilterData filterData);
 }
