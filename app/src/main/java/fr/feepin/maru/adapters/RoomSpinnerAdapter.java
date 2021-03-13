995188120798bdb09258c613ebf62cda1b94c4be
David DEKEUWER <david.dekeuwer@gmail.com>
2021-03-13 19:20:42 +0100

    feat: implement RoomSpinnerAdapter

--- app/src/main/java/fr/feepin/maru/adapters/RoomSpinnerAdapter.java
+++ app/src/main/java/fr/feepin/maru/adapters/RoomSpinnerAdapter.java
@@ -1,4 +1,49 @@
 package fr.feepin.maru.adapters;
 
-public class RoomSpinnerAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import fr.feepin.maru.models.Room;
+
+public class RoomSpinnerAdapter extends ArrayAdapter<Room> {
+
+    private OnRoomSelectListener listener;
+    private LayoutInflater inflater;
+
+    public RoomSpinnerAdapter(@NonNull Context context, OnRoomSelectListener listener) {
+        super(context, android.R.layout.simple_spinner_item, Room.values());
+        this.listener = listener;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Room room = getItem(position);
+        View view = inflater.inflate(android.R.layout.simple_spinner_item, parent, false);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onRoomSelect(room);
+            }
+        });
+
+        TextView textView = view.findViewById(android.R.id.text1);
+        textView.setText(getContext().getString(room.getRoomName()));
+
+        return view;
+    }
+
+    public interface OnRoomSelectListener {
+        void onRoomSelect(Room room);
+    }
+
 }
