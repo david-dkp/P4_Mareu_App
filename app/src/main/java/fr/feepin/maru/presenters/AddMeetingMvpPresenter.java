d0201642d3b06cb833475dafaf828af42bca7e19
David DEKEUWER <david.dekeuwer@gmail.com>
2021-03-13 19:10:49 +0100

    feat: add methods to AddMeetingMvpPresenter

--- app/src/main/java/fr/feepin/maru/presenters/AddMeetingMvpPresenter.java
+++ app/src/main/java/fr/feepin/maru/presenters/AddMeetingMvpPresenter.java
@@ -1,4 +1,15 @@
 package fr.feepin.maru.presenters;
 
-public interface AddMeetingMvpPresenter {
+import fr.feepin.maru.models.Room;
+import fr.feepin.maru.views.AddMeetingMvpView;
+
+public interface AddMeetingMvpPresenter extends MvpPresenter<AddMeetingMvpView>{
+    void onRoomSelect(Room room);
+    void onParticipantClick(String email);
+    void onAddParticipantClick(String email);
+    void onAddParticipantButtonClick();
+    void onAddMeetingItemClick();
+    void onTimeChange(long timeInMillis);
+    void onScrimClick();
+    void onSubjectChange(String input);
 }
