e42984ba935d2611eec965e986095aaaa18c16a2
David DEKEUWER <david.dekeuwer@gmail.com>
2021-03-13 19:11:06 +0100

    feat: implement AddMeetingPresenter

--- app/src/main/java/fr/feepin/maru/presenters/AddMeetingPresenter.java
+++ app/src/main/java/fr/feepin/maru/presenters/AddMeetingPresenter.java
@@ -1,4 +1,101 @@
 package fr.feepin.maru.presenters;
 
-public class AddMeetingPresenter {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import fr.feepin.maru.data.local.FakeMeetingApiGenerator;
+import fr.feepin.maru.data.local.MeetingApi;
+import fr.feepin.maru.models.Meeting;
+import fr.feepin.maru.models.Room;
+import fr.feepin.maru.views.AddMeetingMvpView;
+
+public class AddMeetingPresenter extends BasePresenter<AddMeetingMvpView> implements AddMeetingMvpPresenter{
+
+    private Room selectedRoom;
+    private long timeInMillis;
+    private ArrayList<String> participants, addParticipants;
+    private String subject;
+
+    public AddMeetingPresenter(MeetingApi meetingApi) {
+        super(meetingApi);
+        participants = new ArrayList<>();
+        addParticipants = new ArrayList<String>(Arrays.asList(FakeMeetingApiGenerator.fakeEmails));
+    }
+
+    @Override
+    public void onAttachView(AddMeetingMvpView view) {
+        super.onAttachView(view);
+        updateAddParticipantList();
+        updateParticipantList();
+    }
+
+    @Override
+    public void onRoomSelect(Room room) {
+        this.selectedRoom = room;
+    }
+
+    @Override
+    public void onParticipantClick(String email) {
+        participants.remove(email);
+        addParticipants.add(email);
+        updateParticipantList();
+        updateAddParticipantList();
+    }
+
+    @Override
+    public void onAddParticipantClick(String email) {
+        participants.add(email);
+        addParticipants.remove(email);
+        getView().toggleAddParticipantView(false);
+        updateParticipantList();
+        updateAddParticipantList();
+    }
+
+    @Override
+    public void onAddParticipantButtonClick() {
+        getView().toggleAddParticipantView(true);
+    }
+
+    @Override
+    public void onAddMeetingItemClick() {
+        Meeting meeting = new Meeting(
+                FakeMeetingApiGenerator.fakeMeetings.length,
+                timeInMillis,
+                selectedRoom,
+                subject,
+                participants
+                );
+
+        getMeetingApi().addMeeting(meeting);
+        getView().closeActivity();
+    }
+
+    @Override
+    public void onCloseItemClick() {
+        getView().closeActivity();
+    }
+
+    @Override
+    public void onSubjectChange(String input) {
+        this.subject = input;
+    }
+
+    @Override
+    public void onScrimClick() {
+        getView().toggleAddParticipantView(false);
+    }
+
+    @Override
+    public void onTimeChange(long timeInMillis) {
+        this.timeInMillis = timeInMillis;
+    }
+
+    private void updateParticipantList() {
+        getView().setParticipantList(participants);
+    }
+
+    private void updateAddParticipantList() {
+        getView().setAddParticipantList(addParticipants);
+    }
 }
