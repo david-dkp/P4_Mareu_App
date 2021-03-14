0e08d8f288406fcffc6b5324ed7910a9c43aa347
David DEKEUWER <david.dekeuwer@gmail.com>
2021-03-14 15:49:06 +0100

    feat: add filtering by room test -> success

--- app/src/androidTest/java/fr/feepin/maru/activities/MeetingListActivityTest.java
+++ app/src/androidTest/java/fr/feepin/maru/activities/MeetingListActivityTest.java
@@ -11,10 +11,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Arrays;
+import java.util.List;
+
 import fr.feepin.maru.R;
 import fr.feepin.maru.actions.MeetingListRecyclerViewActions;
+import fr.feepin.maru.assertions.ListAdapterAssertions;
 import fr.feepin.maru.assertions.RecyclerViewAssertions;
 import fr.feepin.maru.data.local.FakeMeetingApiGenerator;
+import fr.feepin.maru.models.Meeting;
+import fr.feepin.maru.models.Room;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -22,6 +28,7 @@ import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -47,6 +54,21 @@ public class MeetingListActivityTest {
         onView(withId(R.id.fabAddMeeting)).perform(ViewActions.click());
         intended(hasComponent(AddMeetingActivity.class.getName()));
         Intents.release();
+
     }
-    
+
+    @Test
+    public void filteringByRoom_withSuccess() throws InterruptedException {
+        Room roomToFilter = Room.MARIO;
+        List<Meeting> expectedMeetings = Arrays.asList(
+                FakeMeetingApiGenerator.fakeMeetings[9],
+                FakeMeetingApiGenerator.fakeMeetings[3]
+        );
+
+        onView(withId(R.id.filter)).perform(ViewActions.click());
+        onView(withText("Mario")).perform(ViewActions.click());
+        onView(withId(R.id.btnApply)).perform(ViewActions.click());
+        onView(withId(R.id.rvMeetings)).check(ListAdapterAssertions.containsOnly(expectedMeetings));
+    }
+
 }
