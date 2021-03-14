90f560bb5e7321e1a62648dafffd54786c873b9b
David DEKEUWER <david.dekeuwer@gmail.com>
2021-03-14 19:34:44 +0100

    improvement: add MeetingListAdapterAssertions

---
+++ app/src/androidTest/java/fr/feepin/maru/assertions/MeetingListAdapterAssertions.java
@@ -0,0 +1,89 @@
+package fr.feepin.maru.assertions;
+
+import android.util.Log;
+import android.view.View;
+
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.ViewAssertion;
+
+import org.junit.Assert;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import fr.feepin.maru.adapters.MeetingListAdapter;
+import fr.feepin.maru.models.Meeting;
+
+public class MeetingListAdapterAssertions {
+
+    public static CurrentListAssertion containsOnly(List<Meeting> meetings) {
+        return new CurrentListAssertion(meetings);
+    }
+
+    public static HasItemAssertion contains(Meeting meeting) {
+        return new HasItemAssertion(meeting);
+    }
+
+    protected static class ListAdapterAssertion implements ViewAssertion {
+
+        private MeetingListAdapter listAdapter;
+
+        @Override
+        public void check(View view, NoMatchingViewException noViewFoundException) {
+            RecyclerView recyclerView = (RecyclerView) view;
+            if (recyclerView == null) throw noViewFoundException;
+            MeetingListAdapter listAdapter = (MeetingListAdapter) recyclerView.getAdapter();
+            assert listAdapter != null;
+            this.listAdapter = listAdapter;
+        }
+
+        public MeetingListAdapter getListAdapter() {
+            return listAdapter;
+        }
+    }
+
+    private static class CurrentListAssertion extends ListAdapterAssertion {
+
+        private List<Meeting> meetings;
+
+        public CurrentListAssertion(List<Meeting> meetings) {
+            this.meetings = meetings;
+        }
+
+        @Override
+        public void check(View view, NoMatchingViewException noViewFoundException) {
+            super.check(view, noViewFoundException);
+            for (Meeting meeting : meetings) {
+                Meeting meetingInAdapter = getListAdapter().getCurrentList().stream().filter((m) -> {
+                    return m.getId() == meeting.getId();
+                }).limit(1).collect(Collectors.toList()).get(0);
+
+                assert meetingInAdapter != null;
+
+                assert meetingInAdapter.toString().equals(meeting.toString());
+            }
+            assert getListAdapter().getCurrentList().size() == meetings.size();
+        }
+    }
+
+    private static class HasItemAssertion extends ListAdapterAssertion {
+
+        private Meeting meeting;
+
+        public HasItemAssertion(Meeting meeting) {
+            this.meeting = meeting;
+        }
+
+        @Override
+        public void check(View view, NoMatchingViewException noViewFoundException) {
+            super.check(view, noViewFoundException);
+            Meeting meetingInAdapter = getListAdapter().getCurrentList().stream().filter((m) -> {
+                return m.getId() == meeting.getId();
+            }).limit(1).collect(Collectors.toList()).get(0);
+            Log.d("debug", meetingInAdapter.toString());
+            assert meeting.toString().equals(meetingInAdapter.toString());
+        }
+    }
+}
